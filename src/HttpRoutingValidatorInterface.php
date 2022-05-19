257a9b7b47677ffbaee3578d05035ec8ca06f514
hashTag <48600109+terrazza@users.noreply.github.com>
2022-05-19 17:08:04 +0200

    fullfill RoutingValidatorInterface

--- src/HttpRoutingValidatorInterface.php
+++ src/HttpRoutingValidatorInterface.php
@@ -1,13 +1,22 @@
 <?php
 namespace Terrazza\Component\HttpRouting;
 use Terrazza\Component\Http\Request\HttpServerRequestInterface;
-use Terrazza\Component\HttpRouting\Exception\HttpInvalidArgumentException;
 
 interface HttpRoutingValidatorInterface {
     /**
+     * @return HttpRoutingReaderInterface
+     */
+    public function getReader() : HttpRoutingReaderInterface;
+
+    /**
+     * @param HttpRoutingReaderInterface $reader
+     * @return HttpRoutingValidatorInterface
+     */
+    public function setReader(HttpRoutingReaderInterface $reader) : HttpRoutingValidatorInterface;
+
+    /**
      * @param HttpRoute $route
      * @param HttpServerRequestInterface $request
-     * @throws HttpInvalidArgumentException
      */
     public function validate(HttpRoute $route, HttpServerRequestInterface $request) : void;
 }
