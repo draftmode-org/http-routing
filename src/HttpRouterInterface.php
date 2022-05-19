e3b31637736c1be6cfda627de15544cc850e3471
hashTag <48600109+terrazza@users.noreply.github.com>
2022-05-19 19:27:46 +0200

    run psalm tests, too

--- src/HttpRouterInterface.php
+++ src/HttpRouterInterface.php
@@ -1,13 +1,11 @@
 <?php
 namespace Terrazza\Component\HttpRouting;
 use Terrazza\Component\Http\Request\HttpServerRequestInterface;
-use Terrazza\Component\HttpRouting\Exception\HttpInvalidArgumentException;
 
 interface HttpRouterInterface {
     /**
      * @param HttpServerRequestInterface $request
      * @return HttpRoute|null
-     * @throws HttpInvalidArgumentException
      */
     public function getRoute(HttpServerRequestInterface $request) : ?HttpRoute;
 }
--- tests/_Mocks/HttpRoutingValidator.php
+++ tests/_Mocks/HttpRoutingValidator.php
@@ -3,6 +3,7 @@ namespace Terrazza\Component\HttpRouting\Tests\_Mocks;
 use InvalidArgumentException;
 use Terrazza\Component\Http\Request\HttpServerRequestInterface;
 use Terrazza\Component\HttpRouting\HttpRoute;
+use Terrazza\Component\HttpRouting\HttpRoutingReaderInterface;
 use Terrazza\Component\HttpRouting\HttpRoutingValidatorInterface;
 
 class HttpRoutingValidator implements HttpRoutingValidatorInterface {
@@ -11,9 +12,37 @@ class HttpRoutingValidator implements HttpRoutingValidatorInterface {
         $this->exception = $exception;
     }
 
+    public function getReader(): HttpRoutingReaderInterface {
+        return new class implements HttpRoutingReaderInterface {
+
+            public function load(string $yamlFileName): HttpRoutingReaderInterface {
+                return $this;
+            }
+            public function getRoutes(): array {
+                return [];
+            }
+
+            public function getParameterParams(string $routePath, string $routeMethod, string $parametersType): ?array {
+                return null;
+            }
+
+            public function getRequestBodyContents(string $routePath, string $routeMethod): ?array {
+                return null;
+            }
+
+            public function getRequestBodyParams(array $content, string $contentType): array {
+                return [];
+            }
+        };
+    }
+
     public function validate(HttpRoute $route, HttpServerRequestInterface $request) : void {
         if ($this->exception) {
             throw $this->exception;
         }
+    }
+
+    public function setReader(HttpRoutingReaderInterface $reader): HttpRoutingValidatorInterface {
+        return $this;
     }
 }
